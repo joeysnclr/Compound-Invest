c753e523a7f98d389d013acf47f808e8ef17d802
joeysnclr <joey.sinclair02@gmail.com>
2019-08-04 11:04:37 -0700

    updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, jsonify
-import json, os, requests, bs4
+import json, os
 import tools
 
 port = int(os.environ.get('PORT', 5000))
